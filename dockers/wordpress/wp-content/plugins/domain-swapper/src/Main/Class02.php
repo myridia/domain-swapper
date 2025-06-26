1f91c4489e28cef8155c522b1e632b91564594a9
veto <veto@myridia.com>
2025-06-26 18:55:29 +0700

    addPlugin

---
+++ dockers/wordpress/wp-content/plugins/domain-swapper/src/Main/Class02.php
@@ -0,0 +1,317 @@
+<?php
+
+namespace WP\DS\Main;
+
+class Class02
+{
+    private $domains;
+    private $siteurl;
+    private $new_siteurl;
+
+    public function __construct()
+    {
+        $o = get_option(WPDS_OPTION);
+        $this->domains = $o['include'];
+        $this->siteurl = get_option('siteurl');
+        $this->new_siteurl = $this->siteurl;
+        if (isset($_SERVER['HTTP_HOST'])) {
+            if ('' != $_SERVER['HTTP_HOST']) {
+                $new_domain = $_SERVER['HTTP_HOST'];
+            }
+        } elseif (isset($_SERVER['SERVER_NAME'])) {
+            if ('' != $_SERVER['SERVER_NAME']) {
+                $new_domain = $_SERVER['SERVER_NAME'];
+            }
+        }
+
+        if (in_array($new_domain, $this->domains)) {
+            $this->new_siteurl = 'https://'.$new_domain;
+        }
+
+        // error_log($this->new_siteurl);
+
+        if ($o['activate']) {
+            if ($this->new_siteurl != $this->siteurl) {
+                add_filter('option_siteurl', [$this, 'swap_siteurl']);
+                add_filter('style_loader_src', [$this, 'swap_style_loader_src'], 10, 4);
+                add_filter('script_loader_src', [$this, 'swap_script_loader_src'], 10, 4);
+                add_filter('template_directory_uri', [$this, 'swap_template_directory_uri']);
+                add_filter('get_canonical_url', [$this, 'swap_get_canonical_url'], 10, 2);
+                add_filter('pre_get_shortlink', [$this, 'swap_pre_get_shortlink'], 10, 4);
+                add_filter('the_content', [$this, 'swap_the_content'], 1);
+                add_filter('home_url', [$this, 'swap_home_url'], 10, 4);
+                add_filter('site_url', [$this, 'swap_site_url'], 10, 4);
+                add_filter('wp_setup_nav_menu_item', [$this, 'swap_wp_setup_nav_menu_item']);
+                add_filter('plugins_url', [$this, 'swap_plugin_url']);
+                //                add_filter('woocommerce_api_request_url', [$this, 'swap_content_url']);
+                //                add_filter('woocommerce_get_asset_url', [$this, 'swap_wo_asset_url'], 10, 2);
+
+                /*
+                add_filter('do_shortcode_tag', [$this, 'swap_do_shortcode_tag'], 10, 4);
+
+                add_filter('feed_link', [$this, 'swap_feed_link'], 10, 2);
+                add_action('registered_taxonomy', [$this, 'swap_start_relative_url']);
+                add_action('shutdown', [$this, 'swap_end_relative_url']);
+                add_filter('option_siteurl', [$this, 'swap_content_url']);
+                add_filter('get_the_guid', [$this, 'swap_content_url']);
+                add_filter('avatar_defaults', [$this, 'swap_content_url']);
+                add_filter('stylesheet_directory_uri', [$this, 'swap_style_uri']);
+
+                add_filter('template_directory_uri', [$this, 'swap_style_uri']);
+                add_filter('upload_dir', [$this, 'swap_featured_img_url']);
+                add_filter('wp_get_attachment_url', [$this, 'swap_attachment_url']);
+
+                add_filter('wp_update_https_url', [$this, 'update_url_to_https']);
+                add_filter('rest_url', [$this, 'swap_rest_url'], 10, 4);
+                add_filter('rest_route_for_post', [$this, 'swap_rest_route_for_post'], 10, 4);
+
+                */
+                // add_filter('stylesheet_directory', [$this, 'swap_style_uri']);
+
+                // add_filter('wp_default_scripts', [$this, 'swap_default_scripts_domain']);
+                // add_filter('content_url', [$this, 'swap_content_url']);
+                // add_filter('pre_option_home', [$this, 'swap_siteurl']);
+                // add_filter('pre_option_home', [$this, 'swap_siteurl']);
+                // add_filter('pre_option_siteurl', [$this, 'swap_siteurl']);
+                // add_filter('option_home', [$this, 'swap_content_url']);
+            }
+        }
+    }
+
+    public function swap_style_loader_src($url)
+    {
+        $new_url = $url;
+        $src_parse = parse_url($url);
+        if (isset($src_parse['host'])) {
+            $host = $src_parse['scheme'].'://'.$src_parse['host'];
+
+            if (isset($src_parse['port'])) {
+                $host = $host.':'.$src_parse['port'];
+            }
+
+            if ($host == $this->siteurl) {
+                $new_url = str_replace($this->siteurl, $this->new_siteurl, $url);
+            }
+        }
+        $new_url = str_replace('http://', 'https://', $new_url);
+        $new_url = str_replace($this->siteurl, $this->new_siteurl, $new_url);
+
+        return $new_url;
+    }
+
+    public function swap_script_loader_src($url)
+    {
+        $new_url = $url;
+        $src_parse = parse_url($url);
+        if (isset($src_parse['host'])) {
+            $host = $src_parse['scheme'].'://'.$src_parse['host'];
+
+            if (isset($src_parse['port'])) {
+                $host = $host.':'.$src_parse['port'];
+            }
+
+            if ($host == $this->siteurl) {
+                $new_url = str_replace($this->siteurl, $this->new_siteurl, $url);
+            }
+        }
+        $new_url = str_replace('http://', 'https://', $new_url);
+        $new_url = str_replace($this->siteurl, $this->new_siteurl, $new_url);
+
+        return $new_url;
+    }
+
+    public function swap_do_shortcode_tag($output, $tag, $attr, $m)
+    {
+        return $output;
+    }
+
+    public function swap_plugin_url($url)
+    {
+        // https://developer.wordpress.org/reference/hooks/plugin_url
+        $url = str_replace($this->siteurl, $this->new_siteurl, $url);
+
+        return $url;
+    }
+
+    public function swap_wp_setup_nav_menu_item($menu_item)
+    {
+        // https://developer.wordpress.org/reference/hooks/wp_setup_nav_menu_item
+        $menu_item->url = str_replace($this->siteurl, $this->new_siteurl, $menu_item->url);
+
+        return $menu_item;
+    }
+
+    public function swap_site_url($url, $path, $orig_scheme, $blog_id)
+    {
+        // https://developer.wordpress.org/reference/hooks/swap_site_url/
+        if ($this->siteurl == $url) {
+        } else {
+            $url = str_replace('http://', 'https://', $url);
+        }
+
+        return $url;
+    }
+
+    public function swap_home_url($url, $path, $orig_scheme, $blog_id)
+    {
+        // https://developer.wordpress.org/reference/hooks/home_url/
+        if ($this->siteurl == $url) {
+        } else {
+            $url = str_replace($this->siteurl, $this->new_siteurl, $url);
+        }
+
+        return $url;
+    }
+
+    public function swap_the_content($content)
+    {
+        /* https://developer.wordpress.org/reference/hooks/the_content/
+          changed /category url
+         */
+        if (is_singular() && in_the_loop() && is_main_query()) {
+            $new_content = str_replace($this->siteurl, $this->new_siteurl, $content);
+
+            return $new_content;
+        }
+
+        return $content;
+    }
+
+    public function swap_pre_get_shortlink($shortlink, $id, $context, $allow_slugs)
+    {
+        // https://developer.wordpress.org/reference/hooks/pre_get_shortlink/
+        if (0 == strlen($shortlink) && '' != $this->new_siteurl) {
+            $shortlink = $this->new_siteurl;
+        } elseif ('' != $this->new_siteurl) {
+            $shortlink = str_replace($this->siteurl, $this->new_siteurl, $shortlink);
+        }
+
+        return $shortlink;
+    }
+
+    public function swap_get_canonical_url($url, $post)
+    {
+        // https://developer.wordpress.org/reference/hooks/get_canonical_url/
+        // error_log($url);
+        $url = str_replace($this->siteurl, $this->new_siteurl, $url);
+
+        return $url;
+    }
+
+    public function swap_template_directory_uri($url)
+    {
+        // https://developer.wordpress.org/reference/hooks/template_directory_uri/
+        $url = str_replace($this->siteurl, $this->new_siteurl, $url);
+
+        return $url;
+    }
+
+    public function swap_siteurl($url)
+    {
+        if ('' != $this->new_siteurl) {
+            if (!defined('WPDS_CUSTOM_REQUEST_URL')) {
+                define('WPDS_CUSTOM_REQUEST_URL', $this->new_siteurl);
+            }
+
+            if (!defined('WP_SITEURL')) {
+                define('WP_SITEURL', $this->new_siteurl);
+            }
+
+            $url = $this->new_siteurl;
+        }
+        // error_log($url);
+
+        return $url;
+    }
+
+    public function swap_feed_link($output, $feed)
+    {
+        // error_log($output);
+
+        return $output;
+    }
+
+    public function swap_default_scripts_domain($scripts)
+    {
+        return $scripts;
+    }
+
+    public function swap_wo_asset_url($url, $path)
+    {
+        $new_url = str_replace($this->siteurl, $this->new_siteurl, $url);
+
+        return $new_url;
+    }
+
+    public function swap_rest_route_for_post($route, $post)
+    {
+        return $route;
+    }
+
+    public function swap_rest_url($url, $path, $blog_id, $scheme)
+    {
+        $new_url = str_replace($this->siteurl, $this->new_siteurl, $url);
+
+        // $new_url = str_replace($this->siteurl, 'https://th.shock.se', $url);
+        /*
+        error_log($this->siteurl);
+        error_log($this->new_siteurl);
+        error_log($url);
+        error_log($new_url);
+        error_log('xxxxxxxxxxxxxxxxxxxxxx');
+        */
+        return $new_url;
+    }
+
+    public function update_url_to_https($url)
+    {
+        return str_replace('http://', 'https://', $url);
+    }
+
+    public function swap_start_relative_url()
+    {
+        ob_start([$this, 'swap_relative_url']);
+    }
+
+    public function swap_end_relative_url()
+    {
+        @ob_end_flush();
+    }
+
+    public function swap_relative_url($buffer)
+    {
+        $url = esc_url(home_url('/'));
+        $url_relative = wp_make_link_relative($url);
+
+        $url_escaped = str_replace('/', '\/', $url);
+        $url_escaped_relative = str_replace('/', '\/', $url_relative);
+
+        $buffer = str_replace($url, $url_relative, $buffer);
+        $buffer = str_replace($url_escaped, $url_escaped_relative, $buffer);
+
+        return $buffer;
+    }
+
+    public function swap_attachment_url($url)
+    {
+        $var = explode('/wp-content', $url);
+
+        $new_url = str_replace($var[0], $this->new_siteurl, $url);
+
+        return $new_url;
+    }
+
+    public function swap_featured_img_url($url)
+    {
+        $new_url = str_replace($this->siteurl, $this->new_siteurl, $url);
+
+        return $new_url;
+    }
+
+    public function swap_style_uri($url)
+    {
+        $new_url = $this->swap_content_url($url);
+
+        return $new_url;
+    }
+}
