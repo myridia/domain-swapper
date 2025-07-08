bddf2887caa4ea0e5170068afc4d90c6693e6c99
veto <veto@lab.local>
2025-07-09 02:43:25 +0700

    update

--- pages/main.py
+++ pages/main.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
+import platform, time, json, csv, datetime
+import requests, argparse
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-
 env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
-template = env.get_template("index.html")
-buf = template.render(name="Pages")
-out_path = "public/index.html"
 
 
-with open(out_path, "w") as f:
-    f.write(buf)
+class MakePages:
+    def __init__(self):
+        print("...init MakePages")
+
+    def start(self):
+        print("...start")
+        template = env.get_template("index.html")
+        buff = template.render(name="Pages")
+        out_path = "public/index.html"
+        with open(out_path, "w") as f:
+            f.write(buff)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="MakePages",
+        description="Generate Pages",
+        epilog="Text at the bottom of help",
+    )
+    mp = MakePages()
+    mp.start()
