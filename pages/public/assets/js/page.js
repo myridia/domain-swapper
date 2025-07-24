764f8dfb70f49fe7e1a1a09459feb338a53413a6
veto <veto@lab.local>
2025-07-24 08:50:52 +0700

    update

--- pages/public/assets/js/page.js
+++ pages/public/assets/js/page.js
@@ -1,12 +1,22 @@
 var vwu = new Vanilla_website_utils();
-
+var random = Math.floor(Math.random() * 10000000);
 window.onload = async function () {
   let input = document.querySelector("#token");
   if (input) {
-    let url = "http://127.0.0.1:8089/token?page=domain-swapper";
+    let host = await vwu.get_host();
+    let url =
+      "http://127.0.0.1:8089/token?page=domain-swapper&client_host=" +
+      host +
+      "&r=" +
+      random;
     const data = JSON.parse(await vwu.aget_api(url));
     const token = data[0]["token"];
     console.log(token);
     input.value = token;
+  }
+
+  let random_input = document.querySelector("#random");
+  if (random_input) {
+    random_input.value = random;
   }
 };
