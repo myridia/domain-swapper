86f2b007b7cd74e922003e499dafb74a5b4040e2
veto8 <veto@myridia.com>
2025-09-17 09:36:10 +0700

    Update clean.sh

--- test/clean.sh
+++ test/clean.sh
@@ -3,10 +3,6 @@
 docker rm --force `docker ps -qa`
 docker volume rm $(docker volume ls -q --filter dangling=true)
 docker network prune
-<<<<<<< HEAD
-=======
-
->>>>>>> 9641d45ef15d451cc717c3d3fb91b8c45e015dc1
 
 if [ "$1" == "all" ] || [ $# -gt 1 ]; then
   echo "clean all including images"
